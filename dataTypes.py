25b1df0c32c2262776c48eab84593665ff8dfab7
sathya456 <49604266+sathya456@users.noreply.github.com>
2019-04-15 12:28:54 +0000

    Data types in python

---
+++ dataTypes.py
@@ -0,0 +1,17 @@
+a=10
+print(type(a))
+b="sathya"
+print(type(b))
+listSample=[1,2,3,4]
+print(type(listSample))
+tupleSample=(1,2,3,4)
+print(type(tupleSample))
+dictSample={'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print(type(dictSample))
+
+# ----- Output------- 
+# <type 'int'>
+# <type 'str'>
+# <type 'list'>
+# <type 'tuple'>
+# <type 'dict'>
