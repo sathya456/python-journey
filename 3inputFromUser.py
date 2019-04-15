9f4d750540ce4959f4319af2533efa151cbd023e
sathya456 <49604266+sathya456@users.noreply.github.com>
2019-04-15 13:38:12 +0000

    Sample program to get input from the user

---
+++ 3inputFromUser.py
@@ -0,0 +1,7 @@
+a=input("Enter value")
+# By default input will be String
+print(type(a))
+
+#Output
+#Enter value2                                                                                                                   
+#<class 'str'> 
